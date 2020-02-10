9e9dc2773c6471f5a5195c7ebeea842ac355c825
livetechng <livetechng@gmail.com>
2020-02-10 13:09:09 +0100

    HangFire Enabling

---
+++ src/ICMSDemo.Application/HangfireJob/JobScheduler.cs
@@ -0,0 +1,34 @@
+﻿using Abp.BackgroundJobs;
+using Abp.Dependency;
+using Abp.Domain.Repositories;
+using Abp.Domain.Uow;
+using Abp.Organizations;
+using ICMSDemo.WorkingPapers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ICMSDemo.HangfireJob
+{
+    public class ScheduleJob : BackgroundJob<int>, ITransientDependency
+    {
+        private readonly IRepository<WorkingPaper, long> _wpRepository;
+        public OrganizationUnitManager OrganizationUnitManager { get; set; }
+
+        public ScheduleJob(IRepository<WorkingPaper, long> userRepository, IEmailSender emailSender)
+        {
+            _wpRepository = userRepository;
+            _emailSender = emailSender;
+        }
+
+        [UnitOfWork]
+        public override void Execute(SimpleSendEmailJobArgs args)
+        {
+            var departmentCode = await OrganizationUnitManager.GetCodeAsync((long)input.DepartmentId);
+            var senderUser = _userRepository.Get(args.SenderUserId);
+            var targetUser = _userRepository.Get(args.TargetUserId);
+
+            _emailSender.Send(senderUser.EmailAddress, targetUser.EmailAddress, args.Subject, args.Body);
+        }
+    }
+}
---
+++ src/ICMSDemo.EntityFrameworkCore/Migrations/20200210110107_SpinHangfireJob_For_WP.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ICMSDemo.Migrations
+{
+    public partial class SpinHangfireJob_For_WP : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
